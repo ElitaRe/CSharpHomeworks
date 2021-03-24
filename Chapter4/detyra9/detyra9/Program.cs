96807b598b5544b73b61f27e4f91ca0a27404acc
unknown <you@example.com>
2021-03-23 23:51:36 -0700

    ...

--- Chapter4/detyra9/detyra9/Program.cs
+++ Chapter4/detyra9/detyra9/Program.cs
@@ -2,53 +2,19 @@
 
 public class Program
 {
-    public static void Main()
+    static void Main(string[] args)
     {
-        int firstNumber;
-        int secondNumber;
-        int thirdNumber;
-        int forthNumber;
-        int fifthNumber;
-        Console.WriteLine("Input the first number:");
-        string a = Console.ReadLine();
-        if (!int.TryParse(a, out firstNumber))
-        {
-            Console.WriteLine("Value is not a number, try again");
-            a = Console.ReadLine();
+        int sum = 0;
 
-        }
-        Console.WriteLine("Input the second number:");
-        string b = Console.ReadLine();
-        if (!int.TryParse(b, out secondNumber))
+        Console.Write("Enter numbers count: ");
+        int length = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < length; i++)
         {
-            Console.WriteLine("Value is not a number, try again");
-            b = Console.ReadLine();
+            Console.Write("Enter {0} number: ", i + 1);
+            sum += Int32.Parse(Console.ReadLine());
         }
-        Console.WriteLine("Input the third number:");
-        string c = Console.ReadLine();
-        if (!int.TryParse(c, out thirdNumber))
-        {
-            Console.WriteLine("Value is not a number, try again");
-            c = Console.ReadLine();
-        }
-        Console.WriteLine("Input the forth number:");
-        string d = Console.ReadLine();
-        if (!int.TryParse(d, out forthNumber))
-        {
-            Console.WriteLine("Value is not a number, try again");
-            d = Console.ReadLine();
-        }
-        Console.WriteLine("Input the fifth number:");
-        string e = Console.ReadLine();
-        if (!int.TryParse(e, out fifthNumber))
-        {
-            Console.WriteLine("Value is not a number, try again");
-            e = Console.ReadLine();
-        }
-        Console.WriteLine($"The sum of the number is: {Sum1(firstNumber, secondNumber, thirdNumber, forthNumber, fifthNumber)}");
-    }
-    static int Sum1(int a, int b, int c, int d, int e)
-    {
-        return a + b + c + d + e;
+
+        Console.WriteLine("Sum of all numbers is {0}.", sum);
     }
 }
